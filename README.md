9537133f5fb7467d2acbdff7e4af86927e8e4fed
httpmurilo <muriloeduardo1997@outlook.com>
2021-07-31 17:45:33 -0300

    hr-worker

---
+++ README.md
@@ -0,0 +1,13 @@
+Estudos microservices
+
+APIS : 
+
+1. Workers ( API do retorno de trabalhadores).
+
+
+### Endpoints : 
+
+GET : http://localhost:8001/workers
+
+
+
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Murilo', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Eduardo', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Joao', 300.0);
