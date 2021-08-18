57ff69425d5a0bba73b0d3b1850ab81912dfcddd
httpmurilo <muriloeduardo1997@outlook.com>
2021-08-18 20:09:27 -0300

    hr-worker config server client

--- hr-worker/src/main/java/com/murilo/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/murilo/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,21 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    @Value("${test.config}")
+    private String testConfig;
+
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
     @Autowired
     private Environment environment;
     @Autowired
     private WorkerRepository repository;
 
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
